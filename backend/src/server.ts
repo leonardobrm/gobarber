fa1134566413a69bc464d171ea453d6a4d71e711
leonardo <leonardo.brasil59@outlook.com>
2020-10-21 23:19:45 -0300

    :art: [feature/design-pattern] - design-pattern

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,8 @@ import expess from 'express';
 
 const server = expess();
 
-server.get('/', (req, res) => {
-  return res.json({message: 'hello word'})
-})
+server.get('/', (req, res) => res.json({ message: 'hello word' }));
 
 server.listen(3333, () => {
   console.log('🚀🚀 Server Started 🚀🚀');
-})
+});
