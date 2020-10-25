3884e51263801df9ee7798ce13e5a5189193bd15
leonardo <leonardo.brasil59@outlook.com>
2020-10-25 13:59:28 -0300

    :rocket: services e solid

--- backend/src/repositories/AppointmentsRepository.ts
+++ backend/src/repositories/AppointmentsRepository.ts
@@ -7,7 +7,7 @@ interface CreateApoointmentDTO {
   date: Date;
 }
 
-class AppointmentRepository {
+class AppointmentsRepository {
   private appointments: Appointment[];
 
   constructor() {
@@ -34,4 +34,4 @@ class AppointmentRepository {
   }
 }
 
-export default AppointmentRepository;
+export default AppointmentsRepository;
