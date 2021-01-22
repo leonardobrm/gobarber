e59d68733e724ed2cbf861ea369d452b232233fb
Leonardo <leonardo.brasil59@outlook.com>
2021-01-21 23:51:17 -0300

    :rocket: [GOBARBER-10] - Added context api and conection with the gobarber api

---
+++ web/src/context/AuthContext.tsx
@@ -0,0 +1,58 @@
+import React, { createContext, useCallback, useState } from 'react';
+
+import api from '../service/api';
+
+interface AuthState {
+  token: string;
+  user: object;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: object;
+  signIng(credentials: SignInCredentials): Promise<void>;
+}
+
+export const AuthContextData = createContext<AuthContextData>(
+  {} as AuthContextData,
+);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    if (token && user) {
+      return {
+        token,
+        user: JSON.parse(user),
+      };
+    }
+
+    return {} as AuthState;
+  });
+
+  const signIng = useCallback(async ({ email, password }) => {
+    try {
+      const response = await api.post('/sessions', { email, password });
+
+      const { token, user } = response.data;
+      localStorage.setItem('@GoBarber:token', token);
+      localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+      setData({ token, user });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  return (
+    <AuthContextData.Provider value={{ user: data.user, signIng }}>
+      {children}
+    </AuthContextData.Provider>
+  );
+};
